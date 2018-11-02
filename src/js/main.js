58024e895b33d743f7842c0660cda4fc728caa3d
trung171295 <trungk39c@gmail.com>
2018-11-02 14:50:39 +0700

    ser

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+    $(document).ready(function(){
+        $('.single-item').slick();
+      });;
+      var fixMenu = $('.menu').offset().top;
+      $(window).on('scroll',function(){
+          if($(window).scrollTop() > fixMenu){
+              $('.menu').addClass('is-fixed');
+          }
+          else{
+            $('.menu').removeClass('is-fixed');
+          }
+      })
+});
